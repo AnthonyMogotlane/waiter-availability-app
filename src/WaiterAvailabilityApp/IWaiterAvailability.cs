5609aba9daef813e52f0fc875595de05de50cf5a
Anthony <almogotlane@gmail.com>
2023-01-27 16:52:21 +0200

    Updated IWaiterAvailability.cs

--- src/WaiterAvailabilityApp/IWaiterAvailability.cs
+++ src/WaiterAvailabilityApp/IWaiterAvailability.cs
@@ -5,8 +5,10 @@ public interface IWaiterAvailability
 
     public void CurrentUser(string name);
     public string GetName();
+    public List<string> GetWorkingDays();
     public void AddToSchedule(List<int> selectedDays);
     public void GetData();
     Dictionary<string, List<string>> GetSchedule();
     public void ClearSchedule();
+    public void ClearWorkingDays();
 }
