a2b316c0577fdb13d17f3e2f0ecbd2e6eb27a933
Anthony <almogotlane@gmail.com>
2023-01-28 12:54:44 +0200

    Added functionality for a pop-up msg when a schedule is cleared

--- razor-pages/WaiterAvailabilityApp/wwwroot/js/site.js
+++ razor-pages/WaiterAvailabilityApp/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+/* 
+    Schedule page 
+*/
+const clearPop = document.querySelector(".clear-pop");
+const clearBtn = document.querySelector(".clear-btn");
+
+// Hide
+clearPop.style.visibility = "hidden";
+
+// Show - for 3s
+clearBtn.addEventListener("click", () => {
+    clearPop.style.visibility = "visible";
+
+    // setTimeout(() => {
+    //     clearPop.style.visibility = "hidden";   
+    // }, 3000);
+})
+/*--------------------------------------------------------*/
