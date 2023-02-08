452e92f1badb4d642f53ff04d30859519480d873
Anthony <almogotlane@gmail.com>
2023-02-08 12:49:07 +0200

    Added insert weekdays query

---
+++ sql/insertWeekdays.sql
@@ -0,0 +1,9 @@
+-- Populating weekdays
+INSERT INTO weekdays (day) VALUES 
+    ('Monday'),
+    ('Tuesday'),
+    ('Wednesday'),
+    ('Thursday'),
+    ('Friday'),
+    ('Saturday'),
+    ('Sunday');
